a422323ceb8ba63041517f6974f3a755c972813f
Rus0d <6087659a>
2017-10-27 02:37:59 +0300

    .

---
+++ my-app/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import registerServiceWorker from './registerServiceWorker';
+
+
+import App from "./App/App";
+import './index.css';
+import reducer from './App/reducers';
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
+registerServiceWorker();
+
